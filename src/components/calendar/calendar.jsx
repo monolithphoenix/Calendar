f501d8dd463e2bcd3c20e949f54ee7f05e3aa2fd
iRoy <44869601+monolithphoenix@users.noreply.github.com>
2019-07-11 15:26:31 +0300

    c-r-a and create component

---
+++ src/components/calendar/calendar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Calendar extends Component {
+  state = {
+
+  }
+
+
+  render() {
+    return (
+      <div>
+          test
+      </div>
+    );
+  }
+}
+
+export default Calendar;
