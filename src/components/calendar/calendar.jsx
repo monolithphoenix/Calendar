63a6c9677f3495b141527353ca9986998780a54e
Illia Rohovoi <illia.rohovoi@gmail.com>
2019-07-16 16:41:48 +0300

    correct working component with comments

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import styles from "./calendar.module.css"
 
+
 // Названия месяцев
 const monthName=['Январь', 'Февраль',  'Март',  'Апрель', 'Май',  'Июнь',  'Июль',  'Август', 'Сентябрь',  'Октябрь',  'Ноябрь',  'Декабрь'];
  
+
 // Названия дней недели
 const dayName=['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
 
+
 // Текущая дата на момент инициализации
 const currentDate = new Date();
-console.log(currentDate);
+
 
 // Текущий месяц на момент инициализации
 const initialDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-console.log(initialDate);
+
 
 // Функция для создания матрицы чисел месяца
 function getArrayOfDaysInMonth(month, year) {
@@ -22,11 +25,10 @@ function getArrayOfDaysInMonth(month, year) {
   const initialDay = date.getDay() === 0 ? 7 : date.getDay()
 
   for (let i = 1; i < initialDay; i++) {
-    // console.log('push empty elements');
     days[days.length-1].push([])
   }
   while (date.getMonth() === month) {
-    if (date.getDay() === 1) {
+    if (date.getDay() === 1 && days[0].length) {
       days.push([])
     };
     days[days.length-1].push(new Date(date));
@@ -34,43 +36,61 @@ function getArrayOfDaysInMonth(month, year) {
 
   }
   for (let i = date.getDay()-1; i <= 6 && i !==0 ; i++) {
-    // console.log(date.getDay());
     days[days.length-1].push([])
   }
-  // console.log(days);
-  
-  return days;
+
+  return days; // возвращаем матрицу чисел месяца
 }
 
+
+// Модуль календаря >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 class Calendar extends Component {
   state = {
     currentDate: currentDate,
     shownMonth: initialDate,
-    isModalShow: false
+    isModalShow: false,
+    notes: [ {date: "2019,6,1", note: "saved note"} ],
+    modalData: {
+      date: "",
+      note: ""
+    }
   }
 
-  componentDidUpdate = () => {
-    console.log('componentDidUpdate');
-  }
-
+  // Действие по клику на таблицу
   showModal = (e) => {
-    e.target.className === styles.cell && this.setState({isModalShow: true})
+    if (e.target.className === styles.cell) { // проверяем элемент ли это с классом "cell"
+      const cellDate = e.target.dataset.date; // извлекаем значение data-date
+      const hasNote = this.state.notes.find(el => el.date === cellDate); // проверяем имеется ли запись на эту дату
+      this.setState({
+        isModalShow: true, // показываем модальное окно
+        modalData: {
+          date: cellDate, 
+          note:(hasNote && hasNote.note) || ""
+        } // записываем в state данные о оперируемой ячейки
+      })
+    }
   }
+
+  // Закрывааем модальное окно
   hideModal = () => {
     this.setState({isModalShow: false})
   }
   
+  // Возвращаемся на текущий месяц
   getCurrentDate = () => {
     this.setState({shownMonth: initialDate})
   }
 
+  // Перелистываем календарь на предыдущий месяц
   getPrevMonth = () => {
     const year = this.state.shownMonth.getFullYear();
     const month = this.state.shownMonth.getMonth();
     const prevMonth = new Date(Date.UTC(year, month-1));
     this.setState({shownMonth: prevMonth})
   }
+
+  // Перелистываем календарь на следующий месяц
   getNextMonth = () => {
     const year = this.state.shownMonth.getFullYear();
     const month = this.state.shownMonth.getMonth();
@@ -78,45 +98,71 @@ class Calendar extends Component {
     this.setState({shownMonth: nextMonth})
   }
 
+  // Принимаем введённые данные из инпута
+  getInputValue = (e) => {
+    this.setState({
+      modalData: {
+        ...this.state.modalData,
+        note: e.target.value
+      }
+    })
+  }
+
+  // Метод для отрисовки каледаря
   renderCalendar = () => {
     const {shownMonth} = this.state;
     const days = getArrayOfDaysInMonth(shownMonth.getMonth(), shownMonth.getFullYear());
-    // console.log(days);
 
     return (
       <table className={styles.calendar} onClick={this.showModal}>
-        <caption>{monthName[this.state.shownMonth.getMonth()]} {this.state.shownMonth.getFullYear()}</caption>  
+        <caption>{monthName[this.state.shownMonth.getMonth()]} {this.state.shownMonth.getFullYear()}</caption> 
+        <tbody> 
         <tr>
-          <th>{dayName[0]}</th>
-          <th>{dayName[1]}</th>
-          <th>{dayName[2]}</th>
-          <th>{dayName[3]}</th>
-          <th>{dayName[4]}</th>
-          <th>{dayName[5]}</th>
-          <th>{dayName[6]}</th>
+          {dayName.map(el => <th key={el}>{el}</th>)}
         </tr>
-          {days.map(element => <tr>{element.map(el => Array.isArray(el)
-            ? <td></td>
-            : <td className={styles.cell} data-date={`${el.getFullYear()}, ${el.getMonth()}, ${el.getDate()}`}>
-                {el.getDate()}
-              </td>
-          )}
-        </tr>)}
-          
+          { 
+            days.map((element, i) => <tr key={i}>{element.map((el, j) => Array.isArray(el)
+              ? <td key={i.toString()+j}></td>
+              : <td className={styles.cell} data-date={`${el.getFullYear()},${el.getMonth()},${el.getDate()}`} key={el}>
+                  {el.getDate()}
+                </td>
+              )}
+            </tr>)
+          }
+        </tbody> 
       </table>
     )
   }
-  test = (e) => {
-    console.log(new Date(e.target.dataset.date));
+
+  // Действие инициируется при отправке формы(клик на кнопку "Сохранить")
+  saveNotes = (e) => {
+    e.preventDefault(); // предотвращаем перезагрузку страницы
+    const {modalData, notes} = this.state
+    switch (modalData.note) {
+      case "": // если после редактирования в инпут пусто - удалим запись на текущий дени из массива записей
+        this.setState({
+          notes: notes.filter(el => el.date !== modalData.date),
+          modalData: {},
+          isModalShow: false,
+        });
+        break;
+      default: // удаляем предыдущую запись на редактируемую дату(если она существовала) и добавляем обновлённую запись
+        this.setState({
+          notes: [
+            ...notes.filter(el => el.date !== modalData.date),
+            modalData
+          ],
+          modalData: {},
+          isModalShow: false,
+        });
+        break;
+    }
   }
 
 
-
-
   render() {
-    const {currentDate, shownMonth, isModalShow} = this.state;
-    console.log(this.state);
-    
+    const {currentDate, isModalShow, modalData} = this.state;
+    console.log('NEW RENDER', this.state);
     
     return (
       <>
@@ -128,12 +174,20 @@ class Calendar extends Component {
         
         {this.renderCalendar()}
 
-        <button className={styles.btn} onClick={this.showModal}>show Modal</button>
-        <div className={`${styles.modal}${isModalShow ? ' '+styles.modal__show+' '+styles.modal__opasity : ''}`}>
-          <div className={styles.modal_dialog}>
-            <button className={styles.btn} onClick= {this.hideModal}>Close modal</button>
+        {isModalShow && 
+          <div className={`${styles.modal}${isModalShow ? ' '+styles.modal__show+' '+styles.modal__opasity : ''}`}>
+            <form className={styles.modal_dialog} onSubmit={this.saveNotes}>
+              <textarea className={styles.text_area} 
+                placeholder="Здесь можно оставить заметку к этому дню"
+                onChange={this.getInputValue}
+                value={modalData.note}
+                autofocus="true">
+              </textarea>
+              <button className={styles.btn} type="button" onClick={this.hideModal}>Отмена</button>
+              <button className={styles.btn} type="submit">Сохранить</button>
+            </form>
           </div>
-        </div>
+        }
       </>
     );
   }
